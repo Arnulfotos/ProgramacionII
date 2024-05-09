181147c2eb24e47725138c3af529e0ddd6de99ef
ARN <ARN@DESKTOP-J7HGCE3>
2024-05-08 19:48:33 -0700

    Add project files.

---
+++ Clase2/Program.cs
@@ -0,0 +1,46 @@
+﻿string[] listaPeliculas = { "El padrino", "Batman", "Tiempos violentos", "Forrest Gump", "El origen", "Batman 2", "Superman", "El padrino II", "La conversacion" };
+string[] Directores = { "Francis Ford Coppola", "Tim Burton", "Quentin Tarantino", "Robert Zemeckis", "Tim Burton", "Zack Snyder", "Francis Ford Coppola", "Francis Ford Coppola" };
+string valoresEncontrados = "";
+
+ConsoleKeyInfo cki;
+
+//bool encontrada = false;
+do
+{
+    Console.WriteLine("Que pelicula quieres ver?");
+    string peliculaBuscada = Console.ReadLine();
+
+    for (int i = 0; i < listaPeliculas.Length; i++)
+    {
+
+
+
+        if (listaPeliculas[i].ToLower().Contains(peliculaBuscada.ToLower()))
+        {
+            valoresEncontrados += $"\n{listaPeliculas[i]}\n ";
+        }
+
+       
+
+
+        /* if (listaPeliculas[i].ToLower() == peliculaBuscada.ToLower())
+         {
+             Console.WriteLine("Si, es la pelicula #" + (i + 1));
+             encontrada = true;
+             break;
+         } */
+    }
+    if (string.IsNullOrEmpty(valoresEncontrados))
+    {
+        valoresEncontrados += "Busquda sin resultados\n";
+    }
+    Console.WriteLine($"Resultados: \n{valoresEncontrados}");
+    Console.WriteLine("\n---------------------");
+    Console.WriteLine("Press the Escape (Esc) key to quit. \n");
+    Console.WriteLine("Press the Enter to continue... \n");
+
+    valoresEncontrados = "";
+
+    cki = Console.ReadKey();
+
+} while (cki.Key != ConsoleKey.Escape);
