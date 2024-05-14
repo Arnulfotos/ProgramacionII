f26823f6cfcf27fa4feb5122c12c501921071571
arnulfotos <manuelleon1996@gmail.com>
2024-05-13 18:37:35 -0700

    Intro Maxsite

---
+++ Clase2 - MaxSite/Program.cs
@@ -0,0 +1,70 @@
+﻿ConsoleKeyInfo cki;
+
+string[] listaPeliculas = { "El padrino", "Batman", "Tiempos violentos", "Forrest Gump", "El origen", "Batman 2", "Superman", "El padrino II" };
+string[] directores = { "Francis Ford Coppola", "Tim Burton", "Quentin Tarantino", "Robert Zemeckis", "Christopher Nolan", "Tim Burton", "Richard Donner", "Francis Ford Coppola" };
+string[] calificaciones = { "9.2", "7.5", "8.9", "8.8", "8.7", "7.0", "7.3", "9.0" }; //calificaciones de IMDb.
+string[] displayMergeText = new string[8];
+string valoresEncontrados = "";
+
+for (int i = 0; i < listaPeliculas.Length; i++)
+{
+    displayMergeText[i] = $"{listaPeliculas[i]} dirigida por {directores[i]} ({calificaciones[i]} estrellas en IMDb)";
+    //Console.WriteLine(displayMergeText[i]);
+}
+
+
+do
+{
+    Console.WriteLine("Ingrese el dato que desee buscar");
+    string peliculaBuscada = Console.ReadLine();
+
+    for (int i = 0; i < displayMergeText.Length; i++)
+    {
+
+        if (displayMergeText[i].ToLower().Contains(peliculaBuscada.ToLower()))
+        {
+            valoresEncontrados += $"\n{displayMergeText[i]}\n ";
+        }
+
+    }
+
+    if (string.IsNullOrEmpty(valoresEncontrados))
+    {
+        valoresEncontrados += "Busquda sin resultados\n";
+    }
+
+    /*Console.WriteLine("Seleccione la accion que desea\n 1. Buscar por titulo\n 2. Buscar por director\n 3.Buscar por rating\n");
+    cki = Console.ReadKey();
+
+    switch (cki.KeyChar)
+    {
+        case '1':
+            Console.WriteLine("\nUsted desea buscar por titulo");
+            break;
+
+        case '2':
+            Console.WriteLine("\nUsted desea buscar por director");
+            break;
+
+        case '3':
+            Console.WriteLine("\nUsted desea buscar por rating");
+            break;
+
+    } */
+    Console.WriteLine(valoresEncontrados);
+
+
+
+
+    Console.WriteLine("\n---------------------");
+    Console.WriteLine("Presiona la tecla Escape (Esc) para salir.");
+    Console.WriteLine("Presiona Enter para buscar otra película.");
+
+    valoresEncontrados = "";
+
+    cki = Console.ReadKey();
+
+    Console.Clear();
+
+
+}  while (cki.Key != ConsoleKey.Escape) ;
--- Clase2/Program.cs
+++ Clase2/Program.cs
@@ -1,10 +1,10 @@
-﻿string[] listaPeliculas = { "El padrino", "Batman", "Tiempos violentos", "Forrest Gump", "El origen", "Batman 2", "Superman", "El padrino II", "La conversacion" };
+﻿/*string[] listaPeliculas = { "El padrino", "Batman", "Tiempos violentos", "Forrest Gump", "El origen", "Batman 2", "Superman", "El padrino II", "La conversacion" };
 string[] Directores = { "Francis Ford Coppola", "Tim Burton", "Quentin Tarantino", "Robert Zemeckis", "Tim Burton", "Zack Snyder", "Francis Ford Coppola", "Francis Ford Coppola" };
 string valoresEncontrados = "";
 
 ConsoleKeyInfo cki;
 
-//bool encontrada = false;
+bool encontrada = false;
 do
 {
     Console.WriteLine("Que pelicula quieres ver?");
@@ -23,12 +23,12 @@ do
        
 
 
-        /* if (listaPeliculas[i].ToLower() == peliculaBuscada.ToLower())
+        if (listaPeliculas[i].ToLower() == peliculaBuscada.ToLower())
          {
              Console.WriteLine("Si, es la pelicula #" + (i + 1));
              encontrada = true;
              break;
-         } */
+         } 
     }
     if (string.IsNullOrEmpty(valoresEncontrados))
     {
@@ -40,6 +40,40 @@ do
     Console.WriteLine("Press the Enter to continue... \n");
 
     valoresEncontrados = "";
+
+    cki = Console.ReadKey();
+
+} while (cki.Key != ConsoleKey.Escape); */
+
+string[] listaPeliculas = { "El padrino", "Batman", "Tiempos violentos", "Forrest Gump", "El origen", "Batman 2", "Superman", "El padrino II" };
+string[] directores = { "Francis Ford Coppola", "Tim Burton", "Quentin Tarantino", "Robert Zemeckis", "Christopher Nolan", "Tim Burton", "Richard Donner", "Francis Ford Coppola" };
+
+ConsoleKeyInfo cki;
+
+do
+{
+    Console.WriteLine("¿Qué película quieres ver?");
+    string peliculaBuscada = Console.ReadLine();
+    bool encontrada = false;
+
+    for (int i = 0; i < listaPeliculas.Length; i++)
+    {
+        if (listaPeliculas[i].ToLower() == peliculaBuscada.ToLower())
+        {
+            Console.WriteLine($"{listaPeliculas[i]} dirigida por {directores[i]}");
+            encontrada = true;
+            break;
+        }
+    }
+
+    if (!encontrada)
+    {
+        Console.WriteLine("Película no encontrada.");
+    }
+
+    Console.WriteLine("\n---------------------");
+    Console.WriteLine("Presiona la tecla Escape (Esc) para salir.");
+    Console.WriteLine("Presiona Enter para buscar otra película.");
 
     cki = Console.ReadKey();
 
