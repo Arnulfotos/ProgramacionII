1937f44428049e38c29407139d3c57168c692ca5
Arnulfo Leon <manuelleon1996@gmail.com>
2024-05-27 18:30:45 -0700

    rebuild

---
+++ Clase 1 - MiniNetflix/Program.cs
@@ -0,0 +1,38 @@
+﻿//string[] movieRepository = new string[10];
+string[] movieRepository = { "El Padrino", "ET", "Toy Story", "Arrival", "El Conjuro", "Scary Movie", "Hereditary", "Monsters Inc"};
+string busqueda = "";
+ConsoleKeyInfo cki;
+/*for(int i = 0; i < movieRepository.Length; i++)
+{
+    Console.WriteLine("Ingrese el nombre de una pelicula");
+    movieRepository[i] = Console.ReadLine()!;
+    Console.WriteLine();
+
+}*/
+
+
+do
+{
+    Console.WriteLine("Ingrese la pelicula que deseas buscar");
+    busqueda = Console.ReadLine();
+
+    for (int i = 0; i < movieRepository.Length; i++)
+    {
+        if (movieRepository[i].ToLower() == busqueda!.ToLower())
+        {
+            Console.WriteLine($"La pelicula {movieRepository[i]} esta disponible");
+            break;
+        }
+     
+            Console.WriteLine("No encontramos la pelicula que deseas");
+            
+       
+    }
+    Console.WriteLine("Si deas salir persiona ESC...");
+
+    Console.WriteLine("Si deas continuar persiona ENTER...");
+    cki = Console.ReadKey();
+
+
+
+} while (cki.Key != ConsoleKey.Escape);
---
+++ Clase1/Program.cs
@@ -0,0 +1,59 @@
+﻿/*int[] vector = new int[5];
+
+vector = [10, 30, 50, 70, 90];
+
+for (int i = 0; i < vector.Length; i++)
+{
+    Console.WriteLine(vector[i]);   
+} */
+
+int[] vector = new int[5];
+
+double promedio = 0;
+
+
+for (int i = 0; i < vector.Length; i++)
+{
+    do
+    {
+        Console.WriteLine($"Ingrese el valor del vector {i} entre 0 al 100");
+        vector[i] = int.Parse(Console.ReadLine()!);
+
+    } while (vector[i] > 100 || vector[i] < 0);
+}
+
+Console.WriteLine();
+
+
+for (int j = 0; j < vector.Length; j++)
+{
+    Console.WriteLine(vector[j]);
+}
+
+Console.WriteLine();
+
+for(int k = 0; k < vector.Length; k++)
+{
+    promedio += vector[k];
+}
+
+promedio = promedio / vector.Length;
+
+Console.WriteLine($"El promedio final es {promedio}"); 
+
+/*int[] vector = new int[6];
+for (int i = 0; i <= 5; i++)
+{
+    do
+    {
+        Console.WriteLine($"Ingrese un numero del 0 al 100 | Vector {i}");
+        vector[i] = int.Parse(Console.ReadLine());
+        
+    } while (vector[i] < 0 && vector[i] > 100);
+}
+Console.WriteLine("\nContenido del Vector");
+for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine(vector[i]);
+}*/
+
